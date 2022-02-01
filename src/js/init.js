6a8399054b9554516f98a639f92afcfa6659ee26
Aasim Syed <aasimsyed02@outlook.com>
2022-01-31 22:36:58 -0500

    not a problem anymore

--- src/js/init.js
+++ src/js/init.js
@@ -12,13 +12,6 @@ if (!rcpatrol) { // stops multiple instances of RC patrol from running
         /**
          * Initialize variables related to RC patrol
          */
-        if (mw.config.get("skin") == "vector" && !(new URL(location.href)).searchParams.get("useskinversion")) {
-            window.location.replace((function() {
-                var url = (new URL(location.href));
-                url.searchParams.set("useskinversion", "2");
-                return url;
-            })());
-        }
         rcpatrol.changes = [];
         rcpatrol.currentChange = 0;
         window.setInterval(() => {
